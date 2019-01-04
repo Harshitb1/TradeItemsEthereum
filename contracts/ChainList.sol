203ea499d9e065ce3880098fd9f75a9ea03f2690
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2019-01-04 14:20:42 +0530

    sell event contract

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -6,13 +6,18 @@ contract ChainList {
   string description;
   uint price;
 
-
+  event LogSellArticle(
+    address indexed _seller,
+    string _name,
+    uint256 _price
+  );
 
   function sellArticle (string _name, string _description, uint _price) public {
     seller = msg.sender;
     name= _name;
     description = _description;
     price = _price;
+    LogSellArticle(seller, name, price);
 
   }
 
